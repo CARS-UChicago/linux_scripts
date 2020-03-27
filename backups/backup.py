811c8fca738500f2929004ed159e7c13885d30c1
Matthew Newville <newville@cars.uchicago.edu>
2020-03-27 13:06:06 -0500

    more updates, docs, simplify backup script

--- backups/backup.py
+++ backups/backup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#  variation on backup:  back-up from explicit list of folders
+#  incremental or full backup for a list of folders
 #
 #  Matt Newville: U Chicago, 2020
 
@@ -9,48 +9,48 @@ import time
 from filefinder import FileFinder
 
 tarcmd = 'tar czlf '
-newfiles_list = ".NEWFILES"
+newfiles_list = '.NEWFILES'
 ignore_files = ['~$','.o$','.tmp$', '.bak$', 'core\.[0-9]*']
 
-class Backup:
-    def __init__(self, folder_list=None, age=1.1):
-        self.folder_list = folder_list
-        self.age = age
+def incremental_backup(folders, top='.', target='.', age=1.1, tformat='%d'):
+    '''incremental backup for a list of folders
 
-    def incremental_backup(self, folder_list=None, age=None):
-        if folder_list is not None:
-            self.folder_list = folder_list
-        if age is not None:
-            self.age = age
+    Arguments
+    ----------
+    folders    list of folders to backup (relative to top)
+    top        full path to top-level folder ('.')
+    target     full path to target folder ('.')
+    age        age (in days) for incremental backup
+    tformat    format string for time.strformat for output tar file ('%d')
+    '''
+    filelist = []
+    for fname in folders:
+        fname = os.path.abspath(os.path.join(top, fname))
+        finder = FileFinder(top=fname, age=age,
+                            ignore_files=ignore_files)
+        filelist.extend(finder.find_files())
 
-        filelist = []
-        for fname in self.folder_list:
-            print("fname " , fname)
-            finder = FileFinder(top=fname, age=self.age,
-                                ignore_files=ignore_files)
-            filelist.extend(finder.find_files())
+    filelist.append('')
+    with open(newfiles_list, 'w') as fh:
+        fh.write('\n'.join(filelist))
+        fh.flush()
 
+    # make sure file has been written
+    time.sleep(0.5)
 
-        filelist.append('')
-        with open(newfiles_list, 'w') as fh:
-            fh.write('\n'.join(filelist))
-            fh.flush()
+    fname = time.strftime(tformat, time.localtime())
+    fname = os.path.join(target, '%s.tar.gz' % fname)
+    cmd = '%s %s -T %s ' % (tarcmd, fname, newfiles_list)
+    print('#  %s (%d files)' % (cmd, len(filelist)))
+    os.system(cmd)
 
-        # make sure file has been written
-        time.sleep(0.5)
-
-        fname = time.strftime("%b%d", time.localtime())
-        cmd = "%s %s.tar.gz -T %s " % (tarcmd, fname, newfiles_list)
-        print("#  %s" % cmd)
-        os.system(cmd)
-
-    def full_backup(self, folder_list=None):
-        """full backup - all files that are younger than 50,000 days old"""
-        self.incremental_backup(age=50000)
+def full_backup(folders=None, top=None, target=None):
+    '''full backup - all files that are younger than 50,000 days old'''
+    incremental_backup(folders=folders, top=top, target=target,
+                       age=50000, tformat='%Y_%b%d')
 
 
 if __name__ == '__main__':
-    flist = ('bin', 'Desktop', 'Codes')
-    folders = [os.path.abspath(os.path.join('/Users/Newville', f)) for f in flist]
-    backup = Backup(folder_list=folders)
-    backup.incremental_backup(age=1.1)
+    folders = ('bin', 'Desktop', 'Codes')
+    home = os.environ['HOME']
+    incremental_backup(folders=folders, top=home, target=home, age=1.1)
